b15755d53a9df5bce2324c1f7b577643bc7331c1
Dominik Courcelles <dominik.courcelles@gmail.com>
2017-09-11 10:49:22 -0400

    Created empty methods for the different tests.

--- TP1/test/unitTest/GraphGeneratorTest.java
+++ TP1/test/unitTest/GraphGeneratorTest.java
@@ -13,7 +13,13 @@ public class GraphGeneratorTest {
 	private static final String COMPLETE_TYPE_NAME = "complete";
 	
 	@Test
-	public void simple()
+	public void simple1()
+	{
+		
+	}
+	
+	@Test
+	public void simple2()
 	{
 		Graph graphGenerated = GraphGenerator.simple(5, 0.7);
 		assertEquals("Should be a graph of type complete.", SIMPLE_TYPE_NAME, graphGenerated.getTypeName());
@@ -25,4 +31,75 @@ public class GraphGeneratorTest {
 		GraphGenerator.complete(-1);
 	}
 
+	@Test
+	public void completeBipartie()
+	{
+		
+	}
+	
+	@Test
+	public void bipartite1()
+	{
+		
+	}
+	
+	@Test
+	public void bipartite2()
+	{
+		
+	}
+	
+	@Test
+	public void path()
+	{
+		
+	}
+	
+	@Test
+	public void binaryTree()
+	{
+		
+	}
+	
+	@Test
+	public void cycle()
+	{
+		
+	}
+	
+	@Test
+	public void eulerianCycle()
+	{
+		
+	}
+	
+	@Test
+	public void eulerianPath()
+	{
+		
+	}
+	
+	@Test
+	public void wheel()
+	{
+		
+	}
+	
+	@Test
+	public void star()
+	{
+		
+	}
+	
+	@Test
+	public void regular()
+	{
+		
+	}
+	
+	@Test
+	public void tree()
+	{
+		
+	}
 }
