80e3159319144c52327b635fa48197e56f89295a
Loic Lerat <lolera@gigl.polymtl.ca>
2017-09-11 09:53:24 -0400

    first commit

---
+++ TP1/algs4/test/unitTest/GraphGeneratorTest.java
@@ -0,0 +1,14 @@
+package unitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GraphGeneratorTest {
+
+	@Test
+	public void testSimple() {
+		System.out.println("Hello");
+	}
+
+}
