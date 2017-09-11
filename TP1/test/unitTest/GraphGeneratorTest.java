96001012c826ff13652bd9bb85bee363fb05e0d0
Dominik Courcelles <dominik.courcelles@gmail.com>
2017-09-11 10:43:52 -0400

    First two tests created.

--- TP1/test/unitTest/GraphGeneratorTest.java
+++ TP1/test/unitTest/GraphGeneratorTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.GraphGenerator;
+
 public class GraphGeneratorTest {
 
+	private static final String SIMPLE_TYPE_NAME = "simple";
+	private static final String COMPLETE_TYPE_NAME = "complete";
+	
 	@Test
-	public void testSimple() {
-		System.out.println("Hello");
+	public void simple()
+	{
+		Graph graphGenerated = GraphGenerator.simple(5, 0.7);
+		assertEquals("Should be a graph of type complete.", SIMPLE_TYPE_NAME, graphGenerated.getTypeName());
+	}
+	
+	@Test ( expected = IllegalArgumentException.class )
+	public void complete()
+	{
+		GraphGenerator.complete(-1);
 	}
 
 }
