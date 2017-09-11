367f0bce70a9ed916aa04b3a252e4c7a893507a0
Dominik Courcelles <dominik.courcelles@gmail.com>
2017-09-11 11:34:35 -0400

    Added a verification of the number of vertices created for each type of graph.

--- TP1/src/test/java/unitTest/GraphGeneratorTest.java
+++ TP1/src/test/java/unitTest/GraphGeneratorTest.java
@@ -26,8 +26,10 @@ public class GraphGeneratorTest {
 	@Test
 	public void simple1()
 	{
-		Graph graphGenerated = GraphGenerator.simple(5, 2);
+		final int nbOfVertices = 5;
+		Graph graphGenerated = GraphGenerator.simple(nbOfVertices, 2);
 		assertEquals("Should be a graph of type simple.", SIMPLE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test ( expected = IllegalArgumentException.class )
@@ -39,91 +41,120 @@ public class GraphGeneratorTest {
 	@Test
 	public void complete()
 	{
-		Graph graphGenerated = GraphGenerator.complete(6);
+		final int nbOfVertices = 6;
+		Graph graphGenerated = GraphGenerator.complete(nbOfVertices);
 		assertEquals("Should be a graph of type complete.", COMPLETE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 
 	@Test
 	public void completeBipartie()
 	{
-		Graph graphGenerated = GraphGenerator.completeBipartite(4, 3);
+		final int nbOfVerticesFirst = 4;
+		final int nbOfVerticesSecond = 3;
+		Graph graphGenerated = GraphGenerator.completeBipartite(nbOfVerticesFirst, nbOfVerticesSecond);
 		assertEquals("Should be a graph of type complete bipartite.", COMPLETE_BIPARTITE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVerticesFirst + nbOfVerticesSecond + " vertices created.", nbOfVerticesFirst + nbOfVerticesSecond, graphGenerated.V());
 	}
 	
 	@Test
 	public void bipartite1()
 	{
+		final int nbOfVerticesFirst = 1;
+		final int nbOfVerticesSecond = 2;
 		Graph graphGenerated = GraphGenerator.bipartite(1, 2, 0.8);
 		assertEquals("Should be a graph of type bipartite.", BIPARTITE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVerticesFirst + nbOfVerticesSecond + " vertices created.", nbOfVerticesFirst + nbOfVerticesSecond, graphGenerated.V());
 	}
 	
 	@Test
 	public void bipartite2()
 	{
+		final int nbOfVerticesFirst = 4;
+		final int nbOfVerticesSecond = 5;
 		Graph graphGenerated = GraphGenerator.bipartite(4, 5, 8);
 		assertEquals("Should be a graph of type bipartite.", BIPARTITE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVerticesFirst + nbOfVerticesSecond + " vertices created.", nbOfVerticesFirst + nbOfVerticesSecond, graphGenerated.V());
 	}
 	
 	@Test
 	public void path()
 	{
-		Graph graphGenerated = GraphGenerator.path(8);
+		final int nbOfVertices = 8;
+		Graph graphGenerated = GraphGenerator.path(nbOfVertices);
 		assertEquals("Should be a graph of type path.", PATH_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void binaryTree()
 	{
-		Graph graphGenerated = GraphGenerator.binaryTree(12);
+		final int nbOfVertices = 12;
+		Graph graphGenerated = GraphGenerator.binaryTree(nbOfVertices);
 		assertEquals("Should be a graph of type binary tree.", BINARY_TREE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void cycle()
 	{
-		Graph graphGenerated = GraphGenerator.cycle(8);
+		final int nbOfVertices = 8;
+		Graph graphGenerated = GraphGenerator.cycle(nbOfVertices);
 		assertEquals("Should be a graph of type cycle.", CYCLE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void eulerianCycle()
 	{
-		Graph graphGenerated = GraphGenerator.eulerianCycle(5, 5);
+		final int nbOfVertices = 5;
+		Graph graphGenerated = GraphGenerator.eulerianCycle(nbOfVertices, 5);
 		assertEquals("Should be a graph of type eularian cycle.", EULARIAN_CYCLE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void eulerianPath()
 	{
-		Graph graphGenerated = GraphGenerator.eulerianPath(3, 3);
+		final int nbOfVertices = 3;
+		Graph graphGenerated = GraphGenerator.eulerianPath(nbOfVertices, 3);
 		assertEquals("Should be a graph of type eularian path.", EULARIAN_PATH_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void wheel()
 	{
-		Graph graphGenerated = GraphGenerator.wheel(4);
+		final int nbOfVertices = 4;
+		Graph graphGenerated = GraphGenerator.wheel(nbOfVertices);
 		assertEquals("Should be a graph of type wheel", WHEEL_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void star()
 	{
-		Graph graphGenerated = GraphGenerator.star(5);
+		final int nbOfVertices = 5;
+		Graph graphGenerated = GraphGenerator.star(nbOfVertices);
 		assertEquals("Should be a graph of type star", STAR_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void regular()
 	{
-		Graph graphGenerated = GraphGenerator.regular(5, 6);
+		final int nbOfVertices = 5;
+		Graph graphGenerated = GraphGenerator.regular(nbOfVertices, 6);
 		assertEquals("Should be a graph of type regular", REGULAR_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 	
 	@Test
 	public void tree()
 	{
-		Graph graphGenerated = GraphGenerator.tree(8);
+		final int nbOfVertices = 8;
+		Graph graphGenerated = GraphGenerator.tree(nbOfVertices);
 		assertEquals("Should be a graph of type tree", TREE_TYPE_NAME, graphGenerated.getTypeName());
+		assertEquals("Should have " + nbOfVertices + " vertices created.", nbOfVertices, graphGenerated.V());
 	}
 }
